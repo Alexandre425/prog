92eb4a7e6d6128f92139c5c760a8f8f814a40830
Alexandre Daniel dos Santos Rodrigues <alexandre.d.rodrigues@tecnico.ulisboa.pt>
2018-04-23 01:42:51 +0100

    a

--- LAB3/Runner data struct/struct.c
+++ LAB3/Runner data struct/struct.c
@@ -22,13 +22,17 @@ typedef struct Runner{
 
 void structAlloc(FILE*, Runner**, int*);
 void readFile(FILE*, Runner*, int);
+void firstPlaceByEchelon(Runner*, int);
+void timeByNumber(Runner*, int, int);
 
 int main (void){
 
+  FILE* file = NULL;
   Runner* runners = NULL;
   int size = 0;
+  char c = 0;
+  int dorsalNum = 0;
 
-  FILE* file;
 
   file = fopen("braga17.txt", "r");
   if (file == NULL){
@@ -36,15 +40,62 @@ int main (void){
     exit (EXIT_FAILURE);
   }
 
+  //allocating enough memmory for all participants, and passing their information to said memmory
   structAlloc(file, &runners, &size);
   readFile(file, runners, size);
+
+  firstPlaceByEchelon(runners, size);
+
+  printf("Type 'q' to quit, or 'n' to enter a runner number and get their time\n");
+
+  //infinite loop to get a number or to quit the program
+  while(1){
+
+    c = getchar();
+
+    if (c == 'q')
+      break;
+    if (c == 'n'){
+      printf("Insert the runner number: ");
+      scanf("%d", &dorsalNum);
+      timeByNumber(runners, size, dorsalNum);
+    }
+
+  }
 
   free(runners);
 
   return EXIT_SUCCESS;
 }
 
+//timeByNumber: prints the time a runner (identified by his/her number) took to finish the race
+void timeByNumber(Runner* runners, int size, int dorsalNum){
 
+  for ( ; size > 0; size--)
+    if (dorsalNum == runners[size].dorsalNum){
+      printf("Runner number %d finish time: %dh%dm%ds\n", dorsalNum, runners[size].finishTime.h, runners[size].finishTime.m, runners[size].finishTime.s);
+      return;
+    }
+
+  printf("Could not find runner number %d!\n", dorsalNum);
+}
+
+//firstPlaceByEchelon: prints all the information about the runner in first place in each echelon
+void firstPlaceByEchelon(Runner* runners, int size){
+
+  for ( ; size > 0; size--)
+    if (runners[size].echelonPos == 1){
+      printf("----First place in %s----\n", runners[size].echelon);
+      printf("Name: %s\n", runners[size].name);
+      printf("Global pos.: %d\n", runners[size].globalPos);
+      printf("Dorsal num.: %d\n", runners[size].dorsalNum);
+      printf("Gender: %c\n", runners[size].gender);
+      printf("Time of completion: %dh%dm%ds\n\n", runners[size].finishTime.h, runners[size].finishTime.m, runners[size].finishTime.s);
+    }
+}
+
+
+//readFile: reads the file and organizes it into the difined structures
 void readFile(FILE* file, Runner* runners, int size){
 
   char temp[STRING_SIZE] = {0};
@@ -53,13 +104,15 @@ void readFile(FILE* file, Runner* runners, int size){
   //ignore the first line
   fgets(temp, 200, file);
 
+  //saves all data into structure variables
   for (l = 0; l < size; l++){
     fgets(temp, 200, file);
-    sscanf(temp, "%d\t%s\t%d\t%d\t%s\t%c\t%d:%d:%d", runners[l].globalPos, runners[l].echelon, runners[l].echelonPos, runners[l].dorsalNum,
-      runners[l].name, runners[l].gender, runners[l].finishTime.h, runners[l].finishTime.m, runners[l].finishTime.s);
+    sscanf(temp, "%d\t%s\t%d\t%d\t%[^\t]\t%c\t%d:%d:%d", &runners[l].globalPos, &runners[l].echelon, &runners[l].echelonPos, &runners[l].dorsalNum,
+      &runners[l].name, &runners[l].gender, &runners[l].finishTime.h, &runners[l].finishTime.m, &runners[l].finishTime.s);
   }
 }
 
+//structAlloc: aloccates memmory for all runners' data
 void structAlloc(FILE* file, Runner** runners, int* size){
 
   char temp[STRING_SIZE] = {0};
